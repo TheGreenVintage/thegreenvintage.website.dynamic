be94130adc9dd8e609bc8526dfec9cd8f261b56f
Ferran Basora <fcsonline@gmail.com>
2015-01-14 21:55:24 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var locale = require('locale');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var contact = require('./routes/contact');
+
+var app = express();
+
+var supported = ['en', 'es', 'ca'];
+
+app.use(locale(supported));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', routes);
+app.use('/contact', contact);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
